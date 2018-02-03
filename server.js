72d2375d112614405671f37e690c00834b0c4d82
willwen <will.wen1@gmail.com>
2018-02-03 14:54:28 -0500

    added cookie parsers

--- server.js
+++ server.js
@@ -3,8 +3,38 @@ var path = require('path');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//for sessions
+// var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
+const uuidv4 = require('uuid/v4');
+var session = require('express-session');
 
-app.use(express.static(path.join(__dirname ,'webpage')))
+
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/json
+// app.use(bodyParser.json())
+
+app.use(cookieParser("cookie secret"))
+
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+app.get('/create', (req, res)=>{
+	var randomRoom = uuidv4();
+	console.log(randomRoom)
+	res.end(randomRoom)
+})
+
+app.use('/', express.static(path.join(__dirname ,'webpage/home')))
+app.use('/room', express.static(path.join(__dirname ,'webpage/room')))
 
 
 io.on('connection', function(socket){
