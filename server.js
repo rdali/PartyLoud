40e143f1719806ef680376dd47a92ad331b404c9
willwen <will.wen1@gmail.com>
2018-02-04 01:24:04 -0500

    working for 1 person

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ var cookieParser = require('cookie-parser');
 const uuidv4 = require('uuid/v4');
 var session = require('express-session');
 
+const secret = "xcbKxz3cDs0DEz26O2Mz"
 
-
+var activeRooms = [];
 // // parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }))
 // // parse application/json
 // app.use(bodyParser.json())
 
-app.use(cookieParser("cookie secret"))
+app.use(cookieParser(secret))
 
 
 // Express Session
@@ -26,34 +27,100 @@ app.use(session({
     resave: true
 }));
 
+app.use('/', express.static(path.join(__dirname, 'webpage/home')))
+app.use('/', express.static(path.join(__dirname, 'webpage/room')))
 
-app.get('/create', (req, res)=>{
-	var randomRoom = uuidv4();
-	console.log(randomRoom)
-	res.end(randomRoom)
+app.get('/reset', (req, res) => {
+    res.clearCookie("roomid");
+    // res.clearCookie("roomid");
+
+    res.redirect("/")
 })
 
-app.use('/', express.static(path.join(__dirname ,'webpage/home')))
-app.use('/room', express.static(path.join(__dirname ,'webpage/room')))
+app.get('/create', setCookie,
+    (req, res) => {
+        var roomid = req.cookies.roomid || req.params.roomid;
+        res.redirect('/room?id=' + roomid);
+    });
+
+app.get('/room', (req, res)=>{
+	res.sendFile(path.join(__dirname, 'webpage/room/index.html'));
+});
+
+app.get('/run', (req, res)=>{
+	let requests = activeRooms.reduce((promiseChain, room)=>{
+		return promiseChain.then(()=> new Promise ((resolve)=>
+			{
+				console.log('audio message ' + room);
+				io.emit('audio message ' + room, {command: "play"});
+			}));
+	}, Promise.resolve());
+	requests.then(()=> {res.end("sent");});
+})
 
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function(socket) {
+    console.log('a user connected');
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-   	io.emit('chat message', msg);
-  		// socket.broadcast.emit(msg);
-  });
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        
+        // socket.broadcast.emit(msg);
+    });
 
-  //user is active
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    //user is active
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 });
 
 
+function getCookie(req, res, next) {
+    if (req.cookies && req.cookies.id_token) {
+        console.log(req.cookies.id_token)
+    }
+    next();
+}
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+function setCookie(req, res, next) {
+    // check if client sent cookie
+    var cookieVal = req.cookies.roomid;
+    if (cookieVal === undefined) {
+        cookieVal = uuidv4();
+        res.cookie('roomid', cookieVal, { maxAge: 900000, httpOnly: true })
+    } else {
+        console.log("this guy already has a cookie : " + cookieVal)
+    }
+    req.params.roomid=cookieVal;
+    activeRooms.pushIfNotExist(cookieVal, (other)=>{
+    	return other === cookieVal;
+    });
+
+    next();
+}
+
+
+
+
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
+
+// https://stackoverflow.com/questions/1988349/array-push-if-does-not-exist
+// check if an element exists in array using a comparer function
+// comparer : function(currentElement)
+Array.prototype.inArray = function(comparer) { 
+    for(var i=0; i < this.length; i++) { 
+        if(comparer(this[i])) return true; 
+    }
+    return false; 
+}; 
+
+// adds an element to the array if it does not already exist using a comparer 
+// function
+Array.prototype.pushIfNotExist = function(element, comparer) { 
+    if (!this.inArray(comparer)) {
+        this.push(element);
+    }
+}; 
--- webpage/room/index.js
+++ webpage/room/index.js
@@ -1,29 +1,36 @@
-$(function () {
+$(function() {
     var socket = io();
-    $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
-    });
-  });
+    var roomid = getParameterByName("id")
 
-var audio = document.getElementById("audio")
+    $("#partyId").text(roomid);
+
+    socket.on('audio message ' + roomid,
+        function(msg) {
+          if(msg.command == "play"){
+            play();
+          }
+        });
+});
+
 var play = function() {
+    var audio = document.getElementById("audio")
     audio.play()
 }
 var pause = function() {
+    var audio = document.getElementById("audio")
     audio.pause()
 }
 
-function getVolume() {
-    alert(audio.volume);
-}
-var volumeUp = function() {
-    audio.volume
-}
 var setFullVolume = function() {
     audio.volume = 1.0;
+}
+//Thank you https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript/901144
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
